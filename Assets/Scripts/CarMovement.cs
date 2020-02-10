9cef688f682aa4fff2cdded992d0abd3ea7c73b5
Avery <avecolth@gmail.com>
2020-02-10 10:47:15 -0800

    Lesson1_1C
    all new assests and movements

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+   public float Vspeed = 20;
+    public float Hspeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float VerticalInput = Input.GetAxis("Vertical");
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * Vspeed * VerticalInput);
+       // transform.Translate(Vector3.right * Time.deltaTime * Hspeed * HorizontalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * Hspeed * HorizontalInput);
+    }
+}
